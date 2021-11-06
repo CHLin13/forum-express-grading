5087a9ebd0896a63ec72b8e31df880a2eb11e137
CHLin13 <lch132502@gmail.com>
2021-11-06 22:04:53 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const db = require('./models')
 
@@ -9,7 +11,19 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
-
+app.use(session({
+  secret: 'Secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.error = req.flash('error')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,26 @@ const userController = {
   },
 
   signUP: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10)
-    })
-    .then(() => res.redirect('/signin'))
+    if (req.body.password !== req.body.passwordCheck) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    }
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) {
+          req.flash('error_messages', '信箱重複！')
+          return res.redirect('/signup')
+        }
+        User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10)
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
+        return res.redirect('/signin')
+      })
   },
 
   signInPage: (req, res) => {
