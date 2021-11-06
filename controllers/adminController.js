2bcbfa7399da778aaea6a984c61564424ee2c546
CHLin13 <lch132502@gmail.com>
2021-11-06 22:43:27 +0800

    Add admin restaurant create form and add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,9 +3,31 @@ const Restaurant = db.Restaurant
 
 const adminController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({raw: true})
-      .then(restaurants => res.render('admin/restaurants', {restaurants}))
-    
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+
+  },
+
+  createRestaurant: (req, res) => {
+    res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
