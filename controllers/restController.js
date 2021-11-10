850a6020f04f70ea07d00297bd56dd204155381b
CHLin13 <lch132502@gmail.com>
2021-11-10 22:13:20 +0800

    Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -14,6 +14,11 @@ const restController = {
         }))
         res.render('restaurants', { restaurants: data })
       })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
   }
 }
 
