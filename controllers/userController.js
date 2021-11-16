092517f954e8d5b2b2d7899a7dc6fcf8ce26b74b
CHLin13 <lch132502@gmail.com>
2021-11-16 15:51:06 +0800

    Add favorite/unfavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
+const Favorite = db.Favorite
 
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -134,6 +135,24 @@ const userController = {
     //         }
     //       })
     //   })
+  },
+
+  addFavorite: (req, res) => {
+    return Favorite.create({
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    })
+      .then(() => res.redirect('back'))
+  },
+
+  removeFavorite: (req, res) => {
+    return Favorite.destroy({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId
+      }
+    })
+      .then(() => res.redirect('back'))
   }
 }
 
